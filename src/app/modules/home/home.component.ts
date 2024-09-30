ae4d71b5f030bd5be0bbe2b9cd5538b677185390
saulocapistrano <saulocapistrano@gmail.com>
2024-09-30 07:42:12 -0300

    implement request to login

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from 'primeng/api';
+import { AuthRequest } from 'src/app/models/interfacce/user/auth/AuthRequest';
+import { CreateUserRequest } from 'src/app/models/interfacce/user/CreateUserRequest';
+import { CreateUserResponse } from 'src/app/models/interfacce/user/CreateUserResponse';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-home',
@@ -13,22 +19,93 @@ export class HomeComponent {
   loginForm = this.formBuilder.group({
     email: ['', Validators.required],
     password: ['', Validators.required]
-  })
+  });
 
   loginFormRegister = this.formBuilder.group({
     name: ['', Validators.required],
     email: ['', Validators.required],
     password: ['', Validators.required]
-  })
+  });
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private cookService: CookieService,
+    private messageService: MessageService
+  ) {}
 
-  onSubmitLoginForm(): void{
-    console.log('Login Form Data', this.loginForm.validator)
+  onSubmitLoginForm(): void {
+    if (this.loginForm.valid) {
+      this.userService
+        .authUser(this.loginForm.value as AuthRequest)
+        .subscribe({
+          next: (response) => {
+            if (response && response.token) {
+              this.cookService.set('USER_INFO', response.token);
+              this.loginForm.reset();
+
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Sucesso',
+                detail: `Bem vindo de volta, ${response?.name}!`,
+                life: 2000
+              });
+            }
+          },
+          error: (err) => {
+            console.log(err);
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: 'Erro ao fazer login. Verifique suas credenciais.',
+              life: 3000
+            });
+          }
+        });
+    } else {
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Atenção',
+        detail: 'Preencha todos os campos de login corretamente.',
+        life: 3000
+      });
+    }
   }
 
-  onSubmitLoginFormRegister(): void{
-    console.log('Registration Form Data', this.loginFormRegister.validator)
+  onSubmitLoginFormRegister(): void {
+    if (this.loginFormRegister.valid) {
+      this.userService
+        .createUser(this.loginFormRegister.value as CreateUserRequest)
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Sucesso',
+                detail: 'Usuário criado com sucesso!',
+                life: 2000
+              });
+              this.loginFormRegister.reset();
+              this.loginCard = true;
+            }
+          },
+          error: (err) => {
+            console.log(err);
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: 'Erro ao criar usuário. Por favor, tente novamente mais tarde.',
+              life: 3000
+            });
+          }
+        });
+    } else {
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Atenção',
+        detail: 'Preencha todos os campos do formulário de cadastro corretamente.',
+        life: 3000
+      });
+    }
   }
-
-  }
+}
